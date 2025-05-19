dea090dbf158ce488798bd2c88b5ca46376c5a69
CoralArteaga <72654096+CoralArteaga@users.noreply.github.com>
2025-05-20 00:12:46 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,25 +7,20 @@ import Notes from './Notes.jsx'
 import Folders from './Folders.jsx'
 /*import Timer from './Timer.jsx'*/
 import { AuthContextProvider } from './context/UserAuth.jsx'; 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {path:"/",element:<App/>},
-  {path: "/Signin", element:<Signin/>},
-  {path: "/Notes", element: <Notes/>},
-  {path: "/Folders", element: <Folders/>},
-  /*{path:"/Timer", element: <Timer/>}*/
-], {
-  basename: '/coral-arteaga-pr'
-})
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthContextProvider>
-      <RouterProvider router={router} />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/Notes" element={<Notes />} />
+          <Route path="/Folders" element={<Folders />} />
+          {/* <Route path="/Timer" element={<Timer />} /> */}
+        </Routes>
+      </HashRouter>
     </AuthContextProvider>
   </StrictMode>,
 )
-
-
-
