a3f7da78de3e6a2b8123f2ed58f12701e0c72cee
CoralArteaga <72654096+CoralArteaga@users.noreply.github.com>
2025-05-20 00:15:28 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'/coral-arteaga-pr'
+  base:'/coral-arteaga-pr/'
 })
